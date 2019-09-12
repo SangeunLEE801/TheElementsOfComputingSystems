060b3322fde30da62f8bd33ae324d55374c8d796
scarlett.lee.git <scarlett.lee.git@gmail.com>
2019-09-12 21:36:09 +0900

    Add pre-defined symbols

--- withSymbols/src/SymbolTable.java
+++ withSymbols/src/SymbolTable.java
@@ -1,11 +1,38 @@
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SymbolTable {
     private Map<String, Integer> symbolT;
 
     public SymbolTable() {
-        this.symbolT = new HashMap<String, Integer>() {};
+        this.symbolT = Stream.of(
+                new AbstractMap.SimpleImmutableEntry<>("R0", 0),
+                new AbstractMap.SimpleImmutableEntry<>("R1", 1),
+                new AbstractMap.SimpleImmutableEntry<>("R2", 2),
+                new AbstractMap.SimpleImmutableEntry<>("R3", 3),
+                new AbstractMap.SimpleImmutableEntry<>("R4", 4),
+                new AbstractMap.SimpleImmutableEntry<>("R5", 5),
+                new AbstractMap.SimpleImmutableEntry<>("R6", 6),
+                new AbstractMap.SimpleImmutableEntry<>("R7", 7),
+                new AbstractMap.SimpleImmutableEntry<>("R8", 8),
+                new AbstractMap.SimpleImmutableEntry<>("R9", 9),
+                new AbstractMap.SimpleImmutableEntry<>("R10", 10),
+                new AbstractMap.SimpleImmutableEntry<>("R11", 11),
+                new AbstractMap.SimpleImmutableEntry<>("R12", 12),
+                new AbstractMap.SimpleImmutableEntry<>("R13", 13),
+                new AbstractMap.SimpleImmutableEntry<>("R14", 14),
+                new AbstractMap.SimpleImmutableEntry<>("R15", 15),
+                new AbstractMap.SimpleImmutableEntry<>("SP", 0),
+                new AbstractMap.SimpleImmutableEntry<>("LCL", 1),
+                new AbstractMap.SimpleImmutableEntry<>("ARG", 2),
+                new AbstractMap.SimpleImmutableEntry<>("THIS", 3),
+                new AbstractMap.SimpleImmutableEntry<>("THAT", 4),
+                new AbstractMap.SimpleImmutableEntry<>("SCREEN", 16384),
+                new AbstractMap.SimpleImmutableEntry<>("KBD", 24576))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public void addEntry(String symbol, int address) {
