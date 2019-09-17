12c853270d68f30835ee5d14cb136928e88051f6
scarlett.lee.git <scarlett.lee.git@gmail.com>
2019-09-17 20:17:02 +0900

    Add switch case for arithmetic command

--- 7_Virtual_Machine_Part_I/StackArithmetic/src/CodeWriter.java
+++ 7_Virtual_Machine_Part_I/StackArithmetic/src/CodeWriter.java
@@ -13,7 +13,26 @@ public class CodeWriter {
     }
 
     public void writeArithmetic(String command) {
-
+        switch (command) {
+            case "add":
+                break;
+            case "sub":
+                break;
+            case "neg":
+                break;
+            case "eq":
+                break;
+            case "gt":
+                break;
+            case "lt":
+                break;
+            case "and":
+                break;
+            case "or":
+                break;
+            case "not":
+                break;
+        }
     }
 
     public void writePushPop(CommandType command, String segment, int index) {
