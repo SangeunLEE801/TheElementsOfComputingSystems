6daf72ae1e74a664b3e22b861940dedf0c64ba26
scarlett.lee.git <scarlett.lee.git@gmail.com>
2019-09-24 22:33:56 +0900

    Fix wrong assembly not

--- 7_Virtual_Machine_Part_I/StackArithmetic/src/CodeWriter.java
+++ 7_Virtual_Machine_Part_I/StackArithmetic/src/CodeWriter.java
@@ -215,16 +215,15 @@ public class CodeWriter {
                 break;
             case "not":
                 /**
-                 * addr = SP - 1;
-                 * *addr = !(*addr);
+                 * !(*(--SP));
+                 * SP++;
                  */
                 assembly.append("@SP").append("\n")
-                        .append("D=M-1").append("\n")
-                        .append("@addr").append("\n")
-                        .append("M=D").append("\n")
-                        .append("@addr").append("\n")
-                        .append("D=M").append("\n")
-                        .append("M=!D").append("\n");
+                        .append("AM=M-1").append("\n") // A = M - 1
+                        .append("D=M").append("\n") // D = M[M-1]
+                        .append("M=!D").append("\n")
+                        .append("@SP").append("\n") // SP++;
+                        .append("M=M+1").append("\n");
                 break;
         }
         writer.write(assembly.toString());
